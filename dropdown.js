d813e3700525d93cf3f93fe9aca2dd30b6eeed8a
juan_amorim <juan_amorim@estudante.sesisenai.org.br>
2025-11-28 00:28:23 -0300

    V_Esqueleto.S.A

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.dropdown-header').forEach(header => {
+        header.addEventListener('click', () => {
+            header.parentElement.classList.toggle('active');
+        });
+    });
+});
