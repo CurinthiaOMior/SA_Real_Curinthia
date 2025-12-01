40b5316aa3be1b10efff0a99e58cf55c47b9df39
juan_amorim <juan_amorim@estudante.sesisenai.org.br>
2025-12-01 18:39:17 -0300

    versãoTO_QUASE_ACABANDOOOOOOOOO.04.SA

--- scriptBDD.js
+++ scriptBDD.js
@@ -1,24 +1,32 @@
-const btnAdicionar = document.getElementById('btnAdicionar');
-let contador = 1;
+const tabela = document.getElementById('tabela')
+const btnAdicionar = document.getElementById('btnAdicionar')
+
+const nome = document.getElementById('nome')
+const idade = document.getElementById('idade')
+const cidade = document.getElementById('cidade')
+
+let contador = 0
 
 btnAdicionar.addEventListener('click', () => {
-    const nome = document.getElementById('nome');
-    const idade = document.getElementById('idade');
-    const cidade = document.getElementById('cidade');
-    const tabela = document.getElementById('tabela');
-
     if (nome.value && idade.value && cidade.value) {
         tabela.innerHTML += `
-                    <tr>
-                        <td>${contador++}</td>
-                        <td>${nome.value}</td>
-                        <td>${idade.value}</td>
-                        <td>${cidade.value}</td>
-                    </tr>
-                `;
-        nome.value = idade.value = cidade.value = '';
+            <tr>
+                <td>${contador}</td>
+                <td>${nome.value}</td>
+                <td>${idade.value}</td>
+                <td>${cidade.value}</td>
+            </tr>
+            `
+
+        contador++
+
+        nome.value = idade.value = cidade.value = ''
     } else {
-        alert('Preencha todos os campos.');
-        return;
+        alert('Preencha todos os campos.')
+        return
     }
-});
+})
+
+btnLimpar.addEventListener('click', () => {
+    tabela.innerHTML = ``
+})
